06a6729517448b916370457005afdd484a4c06d4
kkuruqt <142188212+Ssamk-kuu-ruu@users.noreply.github.com>
2025-04-15 19:47:02 +0800

    successful chart setup

---
+++ FireApp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from fire.views import HomePageView, ChartView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', HomePageView.as_view(), name='home'),
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart')
+]
