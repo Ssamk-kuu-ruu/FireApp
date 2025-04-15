dbb1002289c396a5e93f48c91bbc77a64ad5ce0c
kkuruqt <142188212+Ssamk-kuu-ruu@users.noreply.github.com>
2025-04-15 20:15:23 +0800

    to be continued by dave

--- FireApp/urls.py
+++ FireApp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from fire.views import HomePageView, ChartView
+from fire.views import HomePageView, ChartView, PieCountbySeverity
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart')
+    path('chart/', PieCountbySeverity, name='chart')
 ]
--- charts/views.py
+++ charts/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.db import connection
+from django.http import JsonResponse
+from django.db.models.functions import ExtractMonth
+
+from django.db.models import Count
+from datetime import datetime
 
 class ChartView(ListView):
     template_name = 'chart.html'
@@ -10,4 +16,22 @@ class ChartView(ListView):
     def get_queryset(self, **kwargs):
         pass
 
-    
+    def PieCountbySeverity(request):
+        query = '''
+        SELECT severity_level, COUNT(*) as count
+        FROM fire_incident
+        GROUP BY severity_level
+        '''
+        data = {}
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            rows = cursor.fetchall()
+
+        if rows:
+            data = {severity: count for severity, count in rows}
+        else:
+            data = {}
+
+        return JsonResponse(data)
+
+
