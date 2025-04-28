79847e49667b1b2db64a176c8d98f08e83d99bce
kkuruqt <142188212+Ssamk-kuu-ruu@users.noreply.github.com>
2025-04-29 05:50:43 +0800

    dynamic multibar chart

--- FireApp/urls.py
+++ FireApp/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart')
-    path('chart/', PieCountbySeverity, name='chart')
-    path('lineChart/', LineCountbyMonth, name='chart')
-    path('muliLineChart/', MultilineIncidentTop3Country, name='chart')
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountbyMonth, name='chart'),
+    path('muliLineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
 ]
