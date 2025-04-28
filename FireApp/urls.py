66591ef9fe88bad00ecd0e1526402334dc7d2b4f
kkuruqt <142188212+Ssamk-kuu-ruu@users.noreply.github.com>
2025-04-29 05:47:14 +0800

    dynamic multiline chart

--- FireApp/urls.py
+++ FireApp/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart')
     path('chart/', PieCountbySeverity, name='chart')
+    path('lineChart/', LineCountbyMonth, name='chart')
+    path('muliLineChart/', MultilineIncidentTop3Country, name='chart')
 ]
